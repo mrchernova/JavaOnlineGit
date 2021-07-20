526775fece64bb89a606e4bd4d63288498ac4556
mrchernova <86979869+mrchernova@users.noreply.github.com>
2021-07-20 15:16:23 +0300

    Update Linear4.java

--- Module1/Linear4.java
+++ Module1/Linear4.java
@@ -1,31 +1,42 @@
+/**
+ * Дано действительное число R вида nnn.ddd (три цифровых разряда в дробной и целой частях). 
+ * Поменять местами дробную и целую части числа и вывести полученное значение числа.
+ */
+
 package by.epam.jonline.module1.linear4;
 
 import java.util.Scanner;
 
 public class Linear4 {
     public static void main(String[] args) {
-        double n;
+        double n = 0;
         double x;
+        boolean flag = false;
 
         Scanner sc = new Scanner(System.in);
-        // check input
         System.out.println("Введите действительное число в формате nnn.ddd ");
 
+        // Проверка на число
         while (!sc.hasNextDouble()) {
             System.out.println("Это не число. Введите число в формате nnn.ddd еще раз");
             sc.next();
         }
 
+        // Проверка на размер числа. Оно должно быть положительным и <= 999.999
         n = sc.nextDouble();
-        
-        if ((n > 1000) & (n <= 0)) {
-            System.out.println("0 <= n < 1000. Попробуйте еще раз");
-            sc.next();
+        while ((n > 999.999) || (n < 0)) {
+            System.out.println("Ваше число >999.999. Попробуйте еще раз ");
+            n = sc.nextDouble();
         }
+        
+        // Обрезка вещественной части до трех знаков
+        n = Math.floor(n * 1000) / 1000;    
+        System.out.format("Вы ввели: %.3f\n", n);
 
-
+        //Обмен местами
         x = (n * 1000) % 1000 + (int) n / 1000.0;
 
+        System.out.print("Результат обмена дробной и целой части: ");
         if (x == 0) {
             System.out.println("000.000");
         } else if (x < 10) {
@@ -35,8 +46,7 @@ public class Linear4 {
         } else {
             System.out.format("%03.3f", x);
         }
-
-
     }
 }
+
 
