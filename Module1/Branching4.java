f36f08c266cddf018ba1dccccb3c4de13af9f45a
mrchernova <86979869+mrchernova@users.noreply.github.com>
2021-07-20 16:42:32 +0300

    Update Branching4.java

--- Module1/Branching4.java
+++ Module1/Branching4.java
@@ -14,8 +14,8 @@ public class Branching4 {
         int y = 5;
         int z = 21;
 
-        System.out.printf("Кирпич %sx%sx%s \n",x,y,z);
-        System.out.printf("Отверстие %sx%s \n",A,B);
+        System.out.printf("Кирпич %sx%sx%s \n", x, y, z);
+        System.out.printf("Отверстие %sx%s \n", A, B);
         
         if (((A >= x) & (B >= y)) || ((A >= y) & (B >= x)) ||
                 ((A >= x) & (B >= z)) || ((A >= z) & (B >= x)) ||
