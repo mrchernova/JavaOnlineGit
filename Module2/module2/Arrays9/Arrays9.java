0f8e3af356e57c1009812efdfa5dc56bb7ed9c41
mrchernova <86979869+mrchernova@users.noreply.github.com>
2021-07-30 16:40:32 +0300

    Update Arrays9.java

--- Module2/module2/Arrays9/Arrays9.java
+++ Module2/module2/Arrays9/Arrays9.java
@@ -2,35 +2,49 @@ package by.epam.jonline.module2.Arrays9;
 
 public class Arrays9 {
     public static void main(String[] args) {
-        
+
         int n = 5;
         int[] a = new int[n];
-        int[] b = new int[n];
+        int[] kol = new int[n];
         int[] c = new int[n];
+
+        int max;
+        int maxkol;
 
         System.out.println("Дан массив:");
         for (int i = 0; i < a.length; i++) {
             a[i] = (int) (Math.random() * 10);
             System.out.print("[" + a[i] + "] ");
         }
-        
-        int count = 0;
 
+        System.out.println();
+        int count = 0;
+        //-------------------------------------------------------------------------------
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length; j++) {
-                if (a[i] == a[j]){
+                if (a[i] == a[j]) {
                     count++;
-                    
-                    
-                }
-                c[i] = a[i];
 
-                b[i] = count;    
+                }
+                c[i] = a[i]; // chislo
+                kol[i] = count; // kolichestvo povtoreniy
             }
-            
-            System.out.print(c[i] + "="+ b[i]+ " ");
+
+            System.out.print(c[i] + "=" + kol[i] + " ");
             count = 0;
         }
-        
+        //-------------------------------------------------------------------------------
+
+        System.out.println();
+        max = kol[0];
+        for (int i = 0; i < kol.length; i++) {
+            if (max < kol[i]) {
+                max = kol[i];
+            }
+            
+
+        }
+        System.out.println(max);
+
     }
 }
