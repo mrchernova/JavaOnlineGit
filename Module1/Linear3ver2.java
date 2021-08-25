398d7de3cf5133c3033dd7bcb4873bb403909ad2
mrchernova <86979869+mrchernova@users.noreply.github.com>
2021-08-25 14:39:49 +0300

    Add files via upload

---
+++ Module1/Linear3ver2.java
@@ -0,0 +1,23 @@
+/**
+ * Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+ * ((sin[x]+cos[y]) / (cos[x]-sin[y])) * tg[xy]
+ */
+
+package by.epam.jonline.module1.linear3;
+
+public class Linear3ver2 {
+    public static void main(String[] args) {
+
+        double x = 30;
+        double y = 30;
+        
+        //double tgxy = (Math.sin(x) / Math.cos(x)) * (Math.sin(y) / Math.cos(y));
+        double xy = x*y;
+        double tgxy = Math.sin(xy) / Math.cos(xy);
+
+        double answer = ((Math.sin(x)+Math.cos(y))*tgxy) / (Math.cos(x)-Math.sin(y));
+        System.out.println(answer);
+        
+       
+    }
+}
