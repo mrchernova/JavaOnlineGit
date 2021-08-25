c07e296458aaa7a517b6255c237a4021f0213cb9
mrchernova <86979869+mrchernova@users.noreply.github.com>
2021-08-25 14:40:46 +0300

    Update Linear3ver2.java

--- Module1/Linear3ver2.java
+++ Module1/Linear3ver2.java
@@ -11,7 +11,6 @@ public class Linear3ver2 {
         double x = 30;
         double y = 30;
         
-        //double tgxy = (Math.sin(x) / Math.cos(x)) * (Math.sin(y) / Math.cos(y));
         double xy = x*y;
         double tgxy = Math.sin(xy) / Math.cos(xy);
 
