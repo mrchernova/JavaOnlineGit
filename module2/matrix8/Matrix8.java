f5687aeb4bad53f0448f38ea44fa3645aef0bf5d
mrchernova <86979869+mrchernova@users.noreply.github.com>
2021-08-31 17:25:00 +0300

    Update Matrix8.java

--- module2/matrix8/Matrix8.java
+++ module2/matrix8/Matrix8.java
@@ -6,14 +6,33 @@
 
 package by.epam.jonline.module2.matrix8;
 
+import java.util.Scanner;
+
 public class Matrix8 {
     public static void main(String[] args) {
 
         int n = 5;
+        int st1;
+        int st2;
         int[][] matrix = new int[n][n];
 
         initMatrix(matrix);
         printMatrix(matrix);
+
+
+        System.out.println("Введите номера столбцов, которые  нужно поменять местами");
+
+        Scanner scSt1 = new Scanner(System.in);
+        System.out.println("Номер первого столбца >>");
+        st1 = scSt1.nextInt();
+
+
+        Scanner scSt2 = new Scanner(System.in);
+        System.out.println("Номер второго столбца >>");
+        st2 = scSt2.nextInt();
+
+        System.out.println("Матрица после замены столбцов:");
+        searchAndReplace(matrix, st1, st2);
     }
 
     public static void initMatrix(int[][] matrix) {
@@ -21,11 +40,40 @@ public class Matrix8 {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
 
-                matrix[i][j] = (int)(Math.random() * 20 - 10);
+                matrix[i][j] = (int) (Math.random() * 20 - 10);
             }
         }
     }
 
+    /*----------------------------------------------------------------------------------------------------------------------*/
+    public static void searchAndReplace(int[][] matrix, int st1, int st2) {
+
+        // Выравнивание для удобства пользователя
+        st1 = st1 - 1;
+        st2 = st2 - 1;
+
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+
+
+                if (j == st1) {
+                    System.out.format("%3s", matrix[i][st2]);
+                } else {
+                    if (j == st2) {
+                        System.out.format("%3s", matrix[i][st1]);
+                    } else {
+                        System.out.format("%3s", matrix[i][j]);
+                    }
+
+                }
+
+            }
+            System.out.println();
+
+        }
+    }
+    /*----------------------------------------------------------------------------------------------------------------------*/
 
     public static void printMatrix(int[][] matrix) {
 
@@ -42,3 +90,19 @@ public class Matrix8 {
 
 
 }
+
+/* 
+ 
+ Scanner sc = new Scanner(System.in);
+        System.out.println("Введите целое положительное число ");
+        
+        while (!sc.hasNextInt()) {
+            System.out.println("Это не целое число. Введите число еще раз");
+            sc.next();
+        }
+            n = sc.nextInt();
+            while (n < 0) {
+                System.out.println("Введите положительное число ");
+                n = sc.nextInt();
+            }
+ */
