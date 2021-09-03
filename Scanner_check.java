5da88176adb8a4aeeaa0869222185251da8123fb
mrchernova <86979869+mrchernova@users.noreply.github.com>
2021-09-03 15:42:44 +0300

    Update Scanner_check.java

--- Scanner_check.java
+++ Scanner_check.java
@@ -1,3 +1,42 @@
+
+
+public static int checkScanner(int n) {
+        int num;
+
+        Scanner scNum = new Scanner(System.in);
+        System.out.println("Номер num >>");
+        while (!scNum.hasNextInt()) {
+            System.out.println("Это не num число. Введите num еще раз");
+            scNum.next();
+        }
+        num = scNum.nextInt();
+
+        boolean ok = false;
+        while (!ok) {
+            if ((num > 0) & (num <= n)) {
+                ok = true;
+            } else {
+                System.out.println("Еще раз номер num");
+                num = scNum.nextInt();
+            }
+        }
+        return num;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Scanner sc = new Scanner(System.in);
 System.out.println("Введите целое положительное число ");
         
