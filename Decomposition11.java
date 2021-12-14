75717587d725d7ea90706a47787fde4d800465fc
mrchernova <86979869+mrchernova@users.noreply.github.com>
2021-12-14 10:16:49 +0300

    Update Decomposition11.java

--- Decomposition11.java
+++ Decomposition11.java
@@ -6,31 +6,28 @@ package by.epam.jonline.module2.decompositions.decomposition11;
 
 public class Decomposition11 {
     public static void main(String[] args) {
-        long a = 12345;
+        long a = 10002345;
         long b = 567891011;
 
-        System.out.println();
+        System.out.println("Даны числа:");
+        System.out.println(a + " и " + b);
+
+        MoreOrLess(a, b);
+    }
+
+    public static void MoreOrLess(long a, long b) {
 
         int ai = Count(a);
         int bi = Count(b);
-
-
-
 
         if (ai == bi)
             System.out.println("В обоих числах одинаковое количество цифр");
         else {
             if (ai > bi)
-                System.out.println("В числе " + a + " цифр больше, чем в "+ b);
+                System.out.println("В числе " + a + " цифр больше");
             else
-                System.out.println("В числе " + b + " цифр больше, чем в " + a);
+                System.out.println("В числе " + b + " цифр больше");
         }
-
-    }
-
-    public static void MoreOrLess(int ai, int bi){
-
-
     }
 
     public static int Count(long x) {
