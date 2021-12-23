424aae67be0db8eaaf0a40b3badee7a9c9c3ab9d
mrchernova <86979869+mrchernova@users.noreply.github.com>
2021-12-23 17:11:13 +0300

    Update Decomposition17.java

--- Decomposition17.java
+++ Decomposition17.java
@@ -10,21 +10,17 @@ import java.util.Scanner;
 public class Decomposition17 {
     public static void main(String[] args) {
 
+        byte k = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите челое число");
-    //    int n = sc.nextInt();
-        int n = 12395349;
-
+        int n = sc.nextInt();
         System.out.println(n);
 
-        int sum = sumDigits(n);
-        System.out.println(sum);
-
-        while (sum >= 10) {
-            sum = sumDigits(sum);
-
-            System.out.println(sum);
+        while (n > 0) {
+            n = n - sumDigits(n);
+            k++;
         }
+        System.out.println("Необходимо произвести "+ k + " действий");
 
 
     }
@@ -32,11 +28,9 @@ public class Decomposition17 {
     public static int sumDigits(int a) {
         int sum = 0;
         int x;
-
         while (a > 0) {
             x = a % 10;
             a = a / 10;
-
             sum += x;
         }
         return sum;
