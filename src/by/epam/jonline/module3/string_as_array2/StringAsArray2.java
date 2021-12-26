697adb209a954938c466568828286e36319afe5d
cherraniram@gmail.com <cherraniram@gmail.com>
2021-12-26 21:03:18 +0300

    decompositions

---
+++ src/by/epam/jonline/module3/string_as_array2/StringAsArray2.java
@@ -0,0 +1,11 @@
+/**
+ * Замените в строке все вхождения 'word' на 'letter'.
+ */
+
+package by.epam.jonline.module3.string_as_array2;
+
+public class StringAsArray2 {
+    public static void main(String[] args) {
+        //   String replStr2 = str.replace("Hello", "Bye"); // Bye world
+    }
+}
