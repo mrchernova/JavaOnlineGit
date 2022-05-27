1d805dfe935adea28acbafcfe152cc73dd5cc6df
mrchernova <86979869+mrchernova@users.noreply.github.com>
2022-05-27 16:10:26 +0300

    Delete src/by/epam/jonline/module3/regex1 directory

--- src/by/epam/jonline/module3/regex1/Step5Sort/SortLexems.java
+++
@@ -1,8 +0,0 @@
-package by.epam.jonline.module3.regex1.Step5Sort;
-
-public class SortLexems {
-    public static void sortLexems(String s) {
-        System.out.println("--------------------------- " + s);
-    }
-
-}
