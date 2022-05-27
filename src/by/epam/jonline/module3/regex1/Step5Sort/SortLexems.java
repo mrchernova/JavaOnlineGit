f52ba59a2985625817e8ab2aa23b12633687cad5
mrchernova <86979869+mrchernova@users.noreply.github.com>
2022-05-27 16:13:36 +0300

    Add files via upload

---
+++ src/by/epam/jonline/module3/regex1/Step5Sort/SortLexems.java
@@ -0,0 +1,8 @@
+package by.epam.jonline.module3.regex1.Step5Sort;
+
+public class SortLexems {
+    public static void sortLexems(String s) {
+        System.out.println("--------------------------- " + s);
+    }
+
+}
