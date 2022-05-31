263b0189c496d71316250a52166c638dcb5adfbb
mrchernova <86979869+mrchernova@users.noreply.github.com>
2022-05-31 16:36:11 +0300

    Delete src/by/epam/jonline/module3/regex1 directory

--- src/by/epam/jonline/module3/regex1/Step5Sort/SortSentences.java
+++
@@ -1,7 +0,0 @@
-package by.epam.jonline.module3.regex1.Step5Sort;
-
-public class SortSentences {
-    public static void sortSentences(String s){
-
-    }
-}
--- src/by/epam/jonline/module3/regex1/Step5Sort/Test/Main.java
+++
@@ -1,8 +0,0 @@
-package by.epam.jonline.module3.regex1.Step5Sort.Test;
-
-public class Main {
-    public static void main(String[] args) {
-        new MyFrame();
-
-    }
-}
